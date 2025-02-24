6d436cfc6d2178a6c0b4fea1d5237cf7b8dba286
Anish <xevenbiswas@gmail.com>
2025-02-24 12:44:06 +0530

    oops

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Component() {
   return (
     <section className="w-full px-2 mx-auto max-w-(--breakpoint-xl) md:px-6 py-12 md:py-24 lg:py-32">
-      <div className="grid border gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] items-center">
+      <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] items-center">
         <div className="flex flex-col justify-center space-y-8">
           <div className="space-y-4">
             <h1 className="text-3xl font-bold tracking-tighter text-balance sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-linear-to-r from-foreground to-muted-foreground/70">
